20a6bc17eebac10f683bde000c56013d26ef64ac
Vincent <li.vincent0@gmail.com>
2020-11-15 23:21:18 -0700

    Dynamically populate options

--- client/src/AugStepForm.js
+++ client/src/AugStepForm.js
@@ -14,17 +14,23 @@ class AugStepForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            stepNumber: 0,  // step number
+            stepNumber: this.props.stepNumber || 0,  // step number
             stepCategory: '',    // step category
             stepName: '',    // step name
             input: '',
             description: '',    // step description, if applicable
             citation: '',   // step citation, if applicable
-            removeStep: false   // determines whether or not this object should be deleted
+            removeStep: false,   // determines whether or not this object should be deleted
+
+            numOfCategories: this.props.numOfCategories || 0,   // number of options in the category dropdown
+            categoriesArray: this.props.categoriesArray || Array(this.props.numOfCategories).fill(null), // array of category options
+            numOfSteps: this.props.numOfSteps || 0, // number of options in the steps dropdown, based on category option
+            stepsArray: this.props.stepsArray || Array(this.props.numOfSteps).fill(null)  // array of step options
         };
 
         this.setRemoveStep = this.setRemoveStep.bind(this);
-
+        this.clearSelects = this.clearSelects.bind(this);
+        this.populateOptions = this.populateOptions.bind(this);
     }
 
     setRemoveStep() {
@@ -32,35 +38,51 @@ class AugStepForm extends React.Component {
         alert('Step deleted');  // for testing
     }
 
+    clearSelects() {
+        if(this.state.stepCategory === '') {
+            document.getElementById('selectStepName').value = '';
+            document.getElementById('inputField').value = '';
+        }
+        if(this.state.stepName === '') {
+            document.getElementById('inputField').value = '';
+        }
+    }
+
+    // returns a returnable form of select options from the array
+    populateOptions(array) {
+        let options = '';
+        for(let i in array) {
+            options += `<option value='${array[i]}'>${array[i]}</option>`;
+        }
+        return options;
+    }
+
     render() {
 
         // select input for step category
-        let selectStepCategory = <Form.Control 
+        let selectStepCategory = <Form.Control id='selectStepCategory'
                                     as='select' 
                                     required='required' 
-                                    onChange={(e) => {this.setState({stepCategory: e.target.value});}} 
+                                    onChange={(e) => {this.setState({stepCategory: e.target.value}); this.clearSelects()}} 
                                     value={this.state.stepCategory}> 
                                         <option value=''>Select a category</option>
-                                        <option value='Extra'>Extra</option>
-
+                                        {this.populateOptions(this.state.categoriesArray)}
                                     </Form.Control>;
 
         // select input for specific step
-        let selectStepName = <Form.Control
+        let selectStepName = <Form.Control id='selectStepName'
                                 as='select'
                                 required='required'
-                                onChange={(e) => {this.setState({stepName: e.target.value});}}
+                                onChange={(e) => {this.setState({stepName: e.target.value}); this.clearSelects()}}
                                 value={this.state.stepName}>
                                     <option value=''>Select a step</option>
-                                    <option value='Extra'>Extra</option>
-                                    
+
                                 </Form.Control>;
 
         // small input field
-        let inputField = <Form.Control 
+        let inputField = <Form.Control id='inputField'
                             type='text' 
-                            placeholder='Input'
-                            disabled={this.state.stepCategory === '' | this.state.stepName === ''}>
+                            placeholder='Input'>
 
                             </Form.Control>
 
@@ -103,7 +125,7 @@ class AugStepForm extends React.Component {
                             {stepCitation}
                         </Form.Row>
                     </Card.Body>
-
+                    {this.populateOptions(this.state.categoriesArray)}
                 </Form.Group>
                 
             </div>
