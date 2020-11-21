bb122cd2e16ae02069f951b627f6ea6737e716ef
Vincent <li.vincent0@gmail.com>
2020-11-20 20:27:17 -0700

    Created parent of PrepStepFormGroups

---
+++ client/src/PrepStepsForm.js
@@ -0,0 +1,24 @@
+/**
+ * PrepStepsForm.js
+ * A Form for the preprocessing steps composed of multile AugStepFormGroups
+ */
+
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './outline.css';
+
+class PrepStepsForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfSteps = 1,
+            steps = []
+        }
+
+        
+    }
+}
+
+export default PrepStepsForm;
