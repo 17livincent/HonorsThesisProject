8bea57c14f5c17f23469c578a4c2205078eb3c2f
Vincent <li.vincent0@gmail.com>
2020-12-17 21:31:17 -0800

    Go-back button, file summary

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
  * The highest-level component of the web app.
  */
 import React from 'react';
-import { Accordion, Card } from 'react-bootstrap';
+import {Accordion, Card, Col, Row, Button, Alert} from 'react-bootstrap';
 
 import InputData from './InputData.js';
 import PrepStepsForm from './PrepStepsForm.js';
@@ -28,7 +28,7 @@ class App extends React.Component {
      * Callback function passed to the InputData
      */
     submitData(files) {
-        alert('CSV files submitted: ' + files);
+        //alert('CSV files submitted: ' + files);
         this.setState({files: files});
         // close this accordion, open the second
         this.setState({currentPanel: '1'});
@@ -36,27 +36,38 @@ class App extends React.Component {
     }
 
     render() {
+        let inputData = <InputData id='main' onSubmit={this.submitData}/>;
+        let prepStepsForm = <PrepStepsForm id='main' />;
+        let goBackButton1 = <Button id='goback1' variant='outline-secondary' onClick={() => (this.setState({currentPanel: '0'}))}>Go back</Button>;
+
+        let displayFiles = this.state.files.length > 0 && this.state.files.map((file) => <React.Fragment>{file.replace('C:\\fakepath\\', '')}<br /></React.Fragment>);   // remove the fakepath if its there
+        let inputDataSummary = <Alert variant='success'><b>Files chosen: </b><br /> {displayFiles}</Alert>;
+
         return (
             <div>
                 <Accordion id='app' activeKey={this.state.currentPanel}>
                     <Card>
                         <Card.Header eventKey='0'>
                             <h2>1. Input Data</h2>
+                            {(this.state.currentPanel !== '0') && inputDataSummary}
                         </Card.Header>
                         <Accordion.Collapse eventKey='0'>
-                            <Card.Body>
-                                <InputData id='main' onSubmit={this.submitData}/>
-                            </Card.Body>
+                            <Card.Body>{inputData}</Card.Body>
                         </Accordion.Collapse>
                     </Card>
                     <Card>
                         <Card.Header eventKey='1'>
-                            <h2>2. Select Preprocessing Steps</h2>
+                            <Row>
+                                <Col>
+                                    <h2>2. Select Preprocessing Steps</h2>
+                                </Col>
+                                <Col>
+                                    {(this.state.currentPanel === '1') && goBackButton1}
+                                </Col>
+                            </Row>
                         </Card.Header>
                         <Accordion.Collapse eventKey='1'>
-                            <Card.Body>
-                                <PrepStepsForm id='main' />
-                            </Card.Body>
+                            <Card.Body>{prepStepsForm}</Card.Body>
                         </Accordion.Collapse>
                     </Card>
                 </Accordion>
