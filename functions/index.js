66c648da2f4e4420af0bc1a280abd459165f96d4
Vincent <li.vincent0@gmail.com>
2020-12-27 22:08:46 -0800

    Fixed 404 page, added port to listen to

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,17 @@ const path = require('path');
 
 const app = express();
 
+const port = 3001;
+
 app.use(express.static('/../client/build'));
 
 app.get('/', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
     response.sendFile(path.join(__dirname, '/../client/build/index.html'));
 });
-/*
-app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '/../client/build/404.html'));
+
+app.listen(port, () => {
+    console.log(`App is listening at port ${port}`);
 });
-*/
+
 exports.app = functions.https.onRequest(app);
