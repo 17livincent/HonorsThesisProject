db8187506f50ad89889eb4e2ea5430f98b035495
Vincent <li.vincent0@gmail.com>
2021-01-02 22:08:06 -0800

    Fixing cors

--- client/src/App.js
+++ client/src/App.js
@@ -57,7 +57,7 @@ class App extends React.Component {
      * 
      */
     commitOps() {
-        const ENDPOINT = 'http://localhost:3000';
+        const ENDPOINT = 'localhost:3000';
         console.log('Files and steps confirmed.');
         let socket = socketIOClient(ENDPOINT);
 
