cf4c2d3d2c6b4231bf5f3430efc190d21dee2087
Vincent <li.vincent0@gmail.com>
2021-02-13 00:12:26 -0700

    Reordered attributes in JSON objects

--- client/src/Transformations.js
+++ client/src/Transformations.js
@@ -43,83 +43,84 @@ class Transformations {
                 {
                     name: 'Normalize', 
                     val: 'norm', 
-                    numOfInputs: 2, 
-                    inputNames: ['Min', 'Max'], 
                     description: 'Rescale the range of the data to be between a min and max.', 
                     citation: '',
+                    numOfInputs: 2, 
+                    inputNames: ['Min', 'Max'], 
                     rules: [(inputs) => (parseFloat(inputs[0]) < parseFloat(inputs[1]))],
                     ruleDescs: ['Normalization minimum must be less than the maximum.']
                 },
                 {
                     name: 'Standardize', 
                     val: 'stand', 
-                    numOfInputs: 0, 
-                    inputNames: [], 
                     description: 'Transform the data to have a mean of 0, and a standard deviation of 1.', 
                     citation: '',
+                    numOfInputs: 0, 
+                    inputNames: [], 
                     rules: [],
                     ruleDescs: []
                 }, 
                 {
                     name: 'Box-Cox transformation',
                     val: 'box-cox',
-                    numOfInputs: 0,
-                    inputNames: [],
                     description: 'Performs a power transformation using the Box-Cox method.  Data are automatically scaled to positive values and returned standardized.  This transformation will not work if data is constant.',
                     citation: '',
+                    numOfInputs: 0,
+                    inputNames: [],
                     rules: [],
                     ruleDescs: []
                 },
                 {
                     name: 'Difference transformation',
                     val: 'dif_trans',
-                    numOfInputs: 0,
-                    inputNames: [],
                     description: 'For all rows, subtracts the value of the previous row from the current row.  NAN\'s are will be dropped.',
                     citation: '',
+                    numOfInputs: 0,
+                    inputNames: [],
                     rules: [],
                     ruleDescs: []
                 },
                 {
                     name: 'Divide standard deviations',
                     val: 'div_stand_devs',
-                    numOfInputs: 0,
-                    inputNames: [],
                     description: 'Divide each column by its standard deviation.',
                     citation: '',
+                    numOfInputs: 0,
+                    inputNames: [],
                     rules: [],
                     ruleDescs: []
                 },
                 {
                     name: 'Moving average smoother',
                     val: 'moving_avg_smoother',
-                    numOfInputs: 1,
-                    inputNames: ['Window size'],
                     description: 'Smooth data by calculating the average out of a defined number of data points.  NaN\'s will be dropped.',
                     citation: '',
+                    numOfInputs: 1,
+                    inputNames: ['Window size'],
                     rules: [(size) => (size[0] > 0), (size) => (Number.isInteger(parseFloat(size[0])))],
                     ruleDescs: ['Filter window size must be greater than 0.', 'Filter window size must be an integer.']
                 },
                 {
                     name: 'Subtract means',
                     val: 'sub_means',
-                    numOfInputs: 0,
-                    inputNames: [],
                     description: 'Subtracts the mean from each column.',
                     citation: '',
+                    numOfInputs: 0,
+                    inputNames: [],
                     rules: [],
                     ruleDescs: []
                 },
                 {
                     name: 'Yeo-Johnson transformation',
                     val: 'y-j',
-                    numOfInputs: 0,
-                    inputNames: [],
                     description: 'Performs a power transformation using the Yeo-Johnson method.  Non-positive values are allowed, and data is returned santdardized.',
                     citation: '',
+                    numOfInputs: 0,
+                    inputNames: [],
                     rules: [],
                     ruleDescs: []
-                }
+                },
+                // add more transformations starting here
             ]
         );
     }
