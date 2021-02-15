13d5aa4fc5839844522272c54205aeac93d72efc
Vincent <li.vincent0@gmail.com>
2021-02-15 16:12:22 -0700

    Updated HomeInfo

--- client/src/HomeInfo.js
+++ client/src/HomeInfo.js
@@ -16,7 +16,10 @@ class HomeInfo extends React.Component {
                     <h1>Welcome!</h1>
                     <p>
                         This site allows you to easily preprocess numerical, time-series data with a code-free interface.  
-                        Simply select CSV files, choose the transformations from the dropdowns in the order you want them to be performed, and download the transformed dataset.
+                        Simply select CSV files, choose the transformations from the dropdowns in the order you want them to be performed, and download the transformed dataset(s) in a zipped folder.
+                    </p>
+                    <p>
+                        For more info, check out the <a href='https://github.com/17livincent/SigNormApp' target='_blank' rel='noreferrer'>Github repository</a>.
                     </p>
                 </div>
             </React.Fragment>
