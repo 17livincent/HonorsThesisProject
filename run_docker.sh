54ff5b39fc9b37fb6ebd734ac074839869e6bfc5
Vincent Li <li.vincent0@gmail.com>
2021-10-25 21:35:01 -0700

    Use host port 80

--- run_docker.sh
+++ run_docker.sh
@@ -2,5 +2,5 @@
 # This script creates and starts the Docker container.
 docker build -t signorm-image .
 docker rm -f signorm-cont
-docker run -d -p 3000:3000 --restart=always --name signorm-cont signorm-image
+docker run -d -p 80:3000 --restart=always --name signorm-cont signorm-image
 docker system prune
